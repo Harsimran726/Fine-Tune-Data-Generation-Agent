f3bafb76f09f4b2ccec1bd7d5ad7fc36a242cde7
Harsimran Singh <harsimransd726@gmail.com>
2025-06-15 19:27:22 +0530

    update cors

--- main.py
+++ main.py
@@ -12,7 +12,15 @@ import os
 from dotenv import load_dotenv
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or your Netlify frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Mount static files
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
